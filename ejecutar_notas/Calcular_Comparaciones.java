4160a7d8e9ad614ca681711f825f9c153cbbc21c
CristianHuertass <cristianhuertas264@gmail.com>
2024-08-28 08:06:50 -0500

    nuevas clases

--- ejecutar_notas/Calcular_Comparaciones.java
+++ ejecutar_notas/Calcular_Comparaciones.java
@@ -10,4 +10,47 @@ package ejecutar_notas;
  */
 public class Calcular_Comparaciones {
     
+    int Estudiantes_arriba;
+   int Estudiantes_abajo;
+   int promedio;
+   int suma;
+   double notas2[];
+   
+   public void establecernotas2(double notas2[]){
+       
+       this.notas2= notas2;
+   }
+   
+   public void Calcular_Compararciones(){
+       
+       for(int i=0; i<notas2.length; i++){
+           
+           suma+= notas2[i];
+       }
+       
+       promedio=suma/notas2.length;
+       
+       for(int i=0; i<notas2.length; i++){
+           
+           if(notas2[i]<promedio){
+               
+               Estudiantes_abajo++;
+           }else{
+               
+               Estudiantes_arriba++;
+           }        
+       }  
+   }
+
+    public int getEstudiantes_arriba() {
+        return Estudiantes_arriba;
+    }
+
+    public int getEstudiantes_abajo() {
+        return Estudiantes_abajo;
+    }
+
+    public int getPromedio() {
+        return promedio;
+    }
 }
--- ejecutar_notas/Ejecutar_Promedio.java
+++ ejecutar_notas/Ejecutar_Promedio.java
@@ -4,10 +4,41 @@
  */
 package ejecutar_notas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author aseso
  */
 public class Ejecutar_Promedio {
     
+     public static void main(String[] args) {
+        
+        int tamaño;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        Calcular_Comparaciones obj = new Calcular_Comparaciones();
+        
+        System.out.println("Digite el tamaño del vector: ");
+        
+        tamaño = entrada.nextInt();
+        
+        double notas []= new double[tamaño];
+        
+        System.out.println("Digite las notas: ");
+        
+        for( int i=0; i<tamaño; i++){
+            
+            notas [i]= entrada.nextDouble();
+        }
+        
+         obj.establecernotas2(notas);
+         obj.Calcular_Compararciones();
+         
+         System.out.println("El promedio de las notas es : "+obj.getPromedio());
+         System.out.println("El numero de estudiantes por arriba del promedio es: "+obj.getEstudiantes_arriba());
+         System.out.println("El numero de estudiantes por abajo del promedio es: "+obj.getEstudiantes_abajo());
+         
+    }
 }
